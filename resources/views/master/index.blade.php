5d187de77a241a6c28756a7199c7fd1d99271d7e
hoangviet1997 <viethh@hasaki.vn>
2020-08-15 23:35:09 +0700

    first commit

---
+++ resources/views/master/index.blade.php
@@ -0,0 +1,366 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="stylesheet" href="css/kara.css">
+    <title>Karaoke Kim Ngư</title>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+</head>
+
+<body style='font-family: "Product_Sans";'>
+    <!--Main Navigation-->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="/">
+                <img src="image/logokimngu.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link waves-effect waves-light text-uppercase text-warning" href="/">Trang Chủ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light text-uppercase text-white" href="#">Giới Thiệu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light text-uppercase text-white" href="#">Sự kiện nổi bật</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle waves-effect waves-light text-uppercase text-white" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tuyển dụng</a>
+                        <!-- <div class="dropdown-menu dropdown-info" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item waves-effect waves-light" href="#">Action</a>
+                            <a class="dropdown-item waves-effect waves-light" href="#">Another action</a>
+                            <a class="dropdown-item waves-effect waves-light" href="#">Something else here</a>
+                        </div> -->
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light text-uppercase text-white" href="#">Tin tức</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light text-uppercase text-white" href="#">Liên hệ</a>
+                    </li>
+                </ul>
+                <form class="form-inline">
+                    <div class="md-form my-0">
+                        <a class="text-danger" href="tel:0972156535" title=""><i class="fa fa-phone" aria-hidden="true"></i> 0972 156 535</a>
+                    </div>
+                    &nbsp; &nbsp; &nbsp;
+                    <div class="md-form my-0">
+                        <a class="text-danger" href="mailto:info@karaokegovap.vn" title=""><i class="fa fa-envelope"></i> info@karaokegovap.vn</a>
+                    </div>
+                    &nbsp; &nbsp; &nbsp;
+                    <div class="md-form my-0">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                    </div>
+                </form>
+            </div>
+        </nav>
+
+        <!--Carousel Wrapper-->
+        <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+            <!--Indicators-->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+                <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+            </ol>
+            <!--/.Indicators-->
+            <!--Slides-->
+            <div class="carousel-inner" role="listbox">
+                <!--First slide-->
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg" alt="First slide">
+                </div>
+                <!--/First slide-->
+                <!--Second slide-->
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg" alt="Second slide">
+                </div>
+                <!--/Second slide-->
+                <!--Third slide-->
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Third slide">
+                </div>
+                <!--/Third slide-->
+            </div>
+            <!--/.Slides-->
+            <!--Controls-->
+            <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            <!--/.Controls-->
+        </div>
+        <!--/.Carousel Wrapper-->
+    </header>
+    <!--Main Navigation-->
+
+    <!--Main layout-->
+    <main class="mt-5">
+
+        <div class="container">
+            <div class="datphong">
+                <form>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <label class="text-warning" for="">Ngày đến</label>
+                            <input class="form-control" type="date" name="" id="">
+                        </div>
+                        <div class="col-md-4">
+                            <label class="text-warning" for="">Giờ đến</label>
+                            <input class="form-control" type="time" name="" id="">
+                        </div>
+                        <div class="col-md-4">
+                            <label class="text-warning" for="">Số người</label>
+                            <input class="form-control" type="number" name="" id="">
+                        </div>
+                        <div class="col-md-12 mt-3">
+                            <div class="text-center">
+                                <button type="" class="btn btn-primary">ĐẶT PHÒNG</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="row art-detail mt-5">
+                <div class="col-md-6">
+                    <h2 class="text-warning">Giới thiệu</h2>
+                    <div class="text-white">
+                        <p>Karaoke Kim Ngư&nbsp;đầu tư lắp đặt các thiết bị kỹ thuật hiện đại được nhập khẩu đồng bộ từ Mĩ, âm thanh của từng điệu nhạc lời ca tại&nbsp;Karaoke Kim Ngư luôn sống động nên mỗi khi lắng nghe người bên cạnh thể hiện tài năng ca hát, bạn không thể không nhịp chân theo tiếng nhạc, nhiệt tình vỗ tay khi bài hát kết thúc và rất có thể bạn sẽ muốn được là người cầm mic tiếp theo.</p>
+                        <p>Đến với Karaoke Kim Ngư, bạn đừng lo không tìm được bài hát phù hợp với chất giọng hay tâm trạng của mình vì ở đây có hơn 40.000 bài hát với các ngôn ngữ Việt, Anh, Trung, Hát Karaoke tiếng nhật để bạn chia sẻ với bạn bè, người thân những cung bậc cảm xúc của mình.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="art-img">
+                        <img class="z-depth-5 rounded" src="image/gioithieu.jpg" alt="" width="100%" height="100%">
+                    </div>
+                </div>
+            </div>
+
+            <div class="list-nhac mt-5">
+                <div class="text-center">
+                    <h2 class="text-warning">Bài hát mới trong tuần</h2>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-sm-6">
+                        <div class="box-black z-depth-5 rounded">
+                            <ul class="list-group bg-black">
+                                <li class="list-group-item d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <i class="fa fa-play" aria-hidden="true"></i>
+                                        <span class="ml-5">Cras justo odio</span>
+                                    </div>
+                                    <span>14</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="box-black z-depth-5 rounded">
+                            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/VfGzRSr41rM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-md-12">
+                    <div class="text-center">
+                        <img src="image/vuongmieng.png" alt="">
+                        <h2 class="text-warning mt-3">NHỮNG VỊ KHÁCH DỄ THƯƠNG CỦA KARAOKE KIM NGƯ</h2>
+                    </div>
+
+                    <div class="row mt-5">
+                        <div class="col-md-4 mt-3">
+                            <img class="z-depth-5 rounded img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(117).jpg">
+                        </div>
+                        <div class="col-md-4 mt-3">
+                            <img class="z-depth-5 rounded img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(117).jpg">
+                        </div>
+                        <div class="col-md-4 mt-3">
+                            <img class="z-depth-5 rounded img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(117).jpg">
+                        </div>
+                        <div class="col-md-4 mt-3">
+                            <img class="z-depth-5 rounded img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(117).jpg">
+                        </div>
+                        <div class="col-md-4 mt-3">
+                            <img class="z-depth-5 rounded img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(117).jpg">
+                        </div>
+                        <div class="col-md-4 mt-3">
+                            <img class="z-depth-5 rounded img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(117).jpg">
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+    </main>
+    <!--Main layout-->
+
+    <!-- Footer -->
+    <button id="myBtn" onclick="topFunction()" title="Go to top">Top</button>
+
+    <footer class="page-footer font-small mdb-color pt-4 bg-black">
+
+        <!-- Footer Links -->
+        <div class="container text-center text-md-left">
+
+            <!-- Footer links -->
+            <div class="row text-center text-md-left mt-3 pb-3">
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1959.2762216618335!2d106.6443245581228!3d10.845521998068707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529bb19abe5c3%3A0x753298f7d98f9c77!2sKaraoke%20Kim%20Ng%C6%B0!5e0!3m2!1svi!2s!4v1597461074925!5m2!1svi!2s" width="300" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
+                <!-- Grid column -->
+
+                <hr class="w-100 clearfix d-md-none">
+
+                <!-- Grid column -->
+                <div class="col-md-5 mx-auto mt-3">
+                    <div class="text-center">
+                        <h6 class="text-uppercase mb-4 font-weight-bold text-warning">Công ty bất động sản rồng dịch vụ và giải trí rồng vàng</h6>
+                    </div>
+                </div>
+                <!-- Grid column -->
+
+                <hr class="w-100 clearfix d-md-none">
+
+                <!-- Grid column -->
+                <div class="col-md-3 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold text-warning">Liên hệ</h6>
+                    <p>
+                        <a class="text-white" href="https://www.google.com/maps/place/Karaoke+Kim+Ng%C6%B0/@10.845522,106.6443246,18z/data=!3m1!4b1!4m5!3m4!1s0x317529bb19abe5c3:0x753298f7d98f9c77!8m2!3d10.845522!4d106.6454189?hl=vi-VN">
+                            <i class="fas fa-home mr-3"></i> 524-526-528 Nguyễn Văn Khối ( Cây Trâm củ ), P9, Quận Gò Vấp,TP.HCM</p>
+                    </a>
+                    <p>
+                        <a class="text-white" href="mailto:info@karaokegovap.vn">
+                            <i class="fas fa-envelope mr-3"></i> info@karaokegovap.vn</p>
+                    </a>
+
+                    <p>
+                        <a class="text-white" href="tel:0972156535">
+                            <i class="fas fa-phone mr-3"></i> 0972 156 535</p>
+                    </a>
+
+                    <p>
+                        <a class="text-white" href="javascipt:void(0);">
+                            <i class="fas fa-clock mr-3"></i> Thời gian mở cửa từ 9:00 am</p>
+                    </a>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Footer links -->
+
+            <hr>
+
+            <!-- Grid row -->
+            <div class="row d-flex align-items-center">
+                <!-- Grid column -->
+                <div class="col"></div>
+                <div class="col">
+                    <!-- Social buttons -->
+                    <div class="text-center text-md-right">
+                        <ul class="list-unstyled list-inline">
+                            <li class="list-inline-item">
+                                <a href="https://www.facebook.com/kimnguktvkaraoke" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                    <i class="fab fa-google-plus-g"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                    <i class="fab fa-linkedin-in"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col"></div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Footer Links -->
+
+    </footer>
+    <!-- Footer -->
+
+    <script>
+        $('.carousel').carousel({
+            interval: 1200,
+            touch: true
+        })
+    </script>
+
+    <script>
+        //Get the button
+        var mybutton = document.getElementById("myBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            window.scroll({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            })
+        }
+    </script>
+
+    <script>
+        let list_song = [{
+            name: "Trời hôm nay nhiều mây cực!",
+            ifame: "",
+            singer: "",
+            active: 1
+        }];
+    </script>
+
+</body>
+
+</html>
